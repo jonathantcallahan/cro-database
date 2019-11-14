586b7c3fc5863e4b00436a232c9d73ed683d3461
Matt Long <MattLong87@gmail.com>
2019-11-14 09:12:50 -0500

    modified dropdown styling

--- cro-database-client/src/components/DropdownFilter.js
+++ cro-database-client/src/components/DropdownFilter.js
@@ -6,29 +6,38 @@ const StyledDropdownFilter = styled.div`
     display: flex;
     flex-direction: column;
     margin-bottom: 20px;
-    border-radius: 5px;
+    border-radius: 10px;
+    box-shadow: 0 5px 12px -2px rgba(0, 0, 0, 0.18), 0 3px 3px -2px rgba(0, 0, 0, 0.24);
 `
 const filterPadding = "20px";
 
 const StyledFilterTitle = styled.span`
     font-weight: bold;
-    background: #eee;
+    background: #ddd;
     padding: ${filterPadding} ${filterPadding} ${filterPadding} ${filterPadding};
     font-weight: 600;
     font-size: 14px;
     letter-spacing: 1px;
     text-transform: uppercase;
     color: #444;
+    border-radius: 10px 10px 0px 0px;
 `
 
 const StyledSelect = styled.select`
     font-weight: bold;
     margin: ${filterPadding} ${filterPadding} ${filterPadding} ${filterPadding};
+    font-size: 16px;
+    font-family: Lato;
+    padding: 6px;
+`
+
+const StyledOption = styled.option`
+    font-family: Lato;
 `
 
 export default class DropdownFilter extends React.Component {
     render() {
-        let options = this.props.options.map((option, key) => <option key={key}>{option}</option>);
+        let options = this.props.options.map((option, key) => <StyledOption key={key}>{option}</StyledOption>);
         //need to handle crash when user selects this option
         options.unshift(<option>-</option>);
         //more human-readable titles for filters. May want to pull this out and use for column names at some point too
